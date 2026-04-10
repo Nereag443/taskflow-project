5b9ee02e8eb2a71df5010eb0c7f4757020af0066
Nereag443 <nerea12172005@gmail.com>
2026-04-10 13:47:55 +0200

    Arreglado el despliegue en Vercel

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const cors = require('cors')
-const path = require('path')
 const swaggerUi = require ('swagger-ui-express')
 const swaggerSpec = require ('./config/swagger')
 
 const app = express()
-const { port } = require('./config/env')
 
 const taskRoutes = require('./routes/task.routes')
 const userPreferencesRoutes = require('./routes/userPreferences')
 
-app.use(cors())
+app.use(cors({
+  origin: 'https://taskflow-project-tailwind-two.vercel.app'
+}))
 app.use(express.json())
 
 app.use('/api/v1/tasks', taskRoutes)
@@ -18,24 +18,14 @@ app.use('/api/v1/user/preferences', userPreferencesRoutes)
 
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
-app.use(express.static(path.join(__dirname, '../../maqueta-tailwind/public')))
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../maqueta-tailwind/public/index.html'))
-})
-
 app.use((err, req, res, next) => {
 
   if (err.message === 'NOT_FOUND') {
-
     return res.status(404).json({
       error: 'Recurso no encontrado'
     })
-
   }
-
   console.error(err)
-
   res.status(500).json({
     error: 'Error interno del servidor'
   })
@@ -43,8 +33,4 @@ app.use((err, req, res, next) => {
 })
 
 
-app.listen(port, () => {
-
-  console.log(`Servidor corriendo en puerto ${port}`)
-
-})
+module.exports = app;
